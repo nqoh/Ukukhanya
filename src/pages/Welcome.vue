7a846354609cc316abdbf2f70824de1b335de654
Nqobile <ngobese.nn@gmail.com>
2026-05-08 14:30:25 +0200

    Done with home page but i will fix some errors later

--- src/pages/Welcome.vue
+++ src/pages/Welcome.vue
@@ -1,9 +1,15 @@
 <template>
-
+   <Hero />
+   <Services />
+   <whychooseus />
+   <Cta />
 </template>
 
 <script setup lang="ts">
-
+import Hero from '../components/App/home/Hero.vue';
+import Services from '../components/App/home/Services.vue';
+import whychooseus from '../components/App/home/whychooseus.vue';
+import Cta from '../components/App/home/Cta.vue';
 </script>
 
 <style scoped>
