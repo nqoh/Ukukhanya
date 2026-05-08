5b026660851155e261fbabbbf52a85a4ba217db2
Nqobile <ngobese.nn@gmail.com>
2026-05-08 13:44:37 +0200

    done with footer but i will edit later to chnage list to links

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -1,9 +1,12 @@
 <template>
-  <AppHeader />
+  <AppHeader /> 
+    <slot />
+  <AppFooter />
 </template>
 
 <script setup lang="ts">
 import AppHeader from '../components/AppHeader.vue';
+import AppFooter from '../components/AppFooter.vue';
 </script>
 
 <style scoped>
